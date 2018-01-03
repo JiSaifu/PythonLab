286dcb82aafeabe9ec66c3a2ad971b724311e19e
saifu.ji <ji.saifu@gmail.com>
2018-01-04 00:29:44 +0900

    Chapter3
    3.2~3.4(一部完了）の実施結果をUPする。

---
+++ chapter3/chapter3.3.py
@@ -0,0 +1,43 @@
+# 3.3 タプル
+# タブるの特徴：
+# 1. イミュータブル（変更不能）
+# 2. 同じ値が複数回収納可能
+
+# 3.3.1 ()を使ったタプルの作成
+print('--- 3.3.1 ---')
+# 空タプルを作る
+empty_tuple = ()
+print(empty_tuple)
+
+# 要素が1個のタプルも末尾にカンマを付けて作る。
+one_marx = 'Groucho',
+print(one_marx)
+
+# 要素が複数ある場合には、すべての要素の後ろにカンマをつける
+# （最後の要素の後ろのカンマは省略できる）
+# 括弧で囲んで定義することで、タプルだとわかりやすくなる。
+marx_tuple = 'Groucho', 'Chico', 'Harpo',
+print(marx_tuple)
+
+# タプルを使えば、一度に複数の変数を代入できる。
+a, b, c = marx_tuple
+print(a, b, c)
+
+# タプルを使えば、一時変数を使わずに一つの分で値を交換できる。
+password = 'swordfish'
+icecream = 'abcdefg'
+password, icecream = icecream, password
+print(password, icecream)
+
+# 変換関数のtuple()を使うとほかのものからタプルを作られる。
+marx_list = ['Groucho', 'Chico', 'Harpo']
+print(tuple(marx_tuple))
+
+# 3.3.2 タプルとリストの比較
+# ・タプルは、消費スペースが小さい
+# ・タプルの要素は、誤って書き換える危険がない
+# ・タプルは辞書のキーとして使える
+# ・名前付きタプル（6.14.1参照）は、オブジェクトの単純な代用品として使える
+# ・関数の引数は、タプルとして渡される（4.7参照）
+
+
---
+++ chapter3/chapter3.4.py
@@ -0,0 +1,68 @@
+# 3.4 辞書
+# 辞書の特徴：
+# 1. 要素の順序が管理されていない、代わりに個々の値に一意なキーを与える。
+# 2. イミュータブル型
+# 3. dictと呼ぶこともある
+
+# 3.4.1 {}による作成
+print('--- 3.4.1 ---')
+empty_dict = {}
+print(empty_dict)
+
+# Pythonでは、リスト、タプル、辞書の最後の要素の後ろにカンマを残しておいてよい。
+bierce = {
+    "day": "A period of twenty-four hours, mostly misspent",
+    "positive": "Mistaken at the top one's voice",
+    "misfortune": "The kind of fortune that never misses",
+}
+print(bierce)
+
+# 3.4.2 dict()を使った変換
+print('--- 3.4.2 ---')
+# 2要素リストのリストを辞書に変換：
+lol = [['a', 'b'], ['c', 'd'], ['e', 'f']]
+print(dict(lol))
+
+# 2要素のタプルのリストを辞書に変換
+lot = [('a', 'b'), ('c', 'd'), ('e', 'f')]
+print(dict(lot))
+
+# 2要素のリストのタプルを辞書に変換
+tol = (['a', 'b'], ['c', 'd'], ['e', 'f'])
+print(dict(tol))
+
+# 2字の文字列（3文字の場合はエラーになる）のリスト
+los = ['ab', 'cd', 'ef']
+print(dict(los))
+
+# 2字の文字列（3文字の場合はエラーになる）のタプル
+tos = ('ab', 'cd', 'ef')
+print(dict(tos))
+
+# 3.4.3 [key]による要素の追加、変更
+print('--- 3.4.3 ---')
+pythons = {
+    'Chapman': 'Graham',
+    'Cleese': 'John',
+    'Idle': 'Eric'
+}
+print(pythons)
+pythons['Gilliam'] = 'Gerry'
+print(pythons)
+pythons['Gilliam'] = 'Terry'
+print(pythons)
+
+# 3.4.4 update()による辞書の結合
+print('--- 3.4.4 ---')
+first = {'a': 1, 'b': 2}
+# second辞書の同キーアイテムはfirst辞書へ更新する
+second = {'b': 'platypus', 'c': 3}
+first.update(second)
+print(first)
+
+# 3.4.5 delによる指定したキーを持つ要素の削除
+print('--- 3.4.5 ---')
+print(pythons)
+del pythons['Gilliam']
+print(pythons)
+
